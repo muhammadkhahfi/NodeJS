b7e96a2063477bf8e37d07c6f77eb5f76633556e
Muhammad Khahfi <muhammad.khahfi@wirecard.com>
2019-05-06 17:31:27 +0700

    update source

--- alert.js
+++ alert.js
@@ -8,21 +8,18 @@ today = mm + dd + yyyy;
 function sendAlert(interval)
 {
 	var request= require('request'); 
-	request('http://localhost:2323/check_nac_status/20190313135721', 
+	var requestString = 'http://localhost:2323/check_nac_status/20190313135721';
+	request(requestString, 
 	function (error, response, body) {
 		
+		console.log('request: ' + requestString);
 		console.log('error:', error); // Print the error if one occurred
 		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-		//console.log('body:', body); // Print the HTML for the Google homepage.		
-		
-		//var obj = JSON.parse(body);
-		//console.log(typeof(obj));
 		
 		if(response && response.statusCode == 200)
 		{
 			var fs = require('fs');
-		
-			//var msg='[' + new Date().toLocaleString() + ']' + ' ' + '[alert.js]: ' + 'Status interval trx timeout: ' + obj.status_interval_trx_timeout + '\n';
+					
 			var msg='[' + new Date().toLocaleString() + ']' + ' ' + '[alert.js]: ' + 'E-mail NAC Status Successfully Send' + '\n';
 			
 			fs.appendFile('Log' + today +'.log', msg , function (err) {
@@ -33,8 +30,7 @@ function sendAlert(interval)
 		else 
 		{			
 			var fs = require('fs');
-		
-			//var msg='[' + new Date().toLocaleString() + ']' + ' ' + '[alert.js]: ' + 'Status interval trx timeout: ' + obj.status_interval_trx_timeout + '\n';
+				
 			var msg='[' + new Date().toLocaleString() + ']' + ' ' + '[alert.js]: ' + 'Error: ' + response && response.statusCode + '\n' + 'body: ' + body + '\n';
 			
 			fs.appendFile('Log' + today +'.log', msg , function (err) {
@@ -51,23 +47,35 @@ request('http://localhost:2323/get_datetime_now',
 function (error, response, body) {
 	
 	console.log('Send alert started...');
-	var rq = 'http://localhost:2323/get_status_email_nac';
-	request(rq, 
+	
+	var requestString = 'http://localhost:2323/get_status_email_nac';
+	request(requestString, 
 	function (error, response, body) {
-	  console.log('Request: ' + rq);
-	  console.log('error:', error); // Print the error if one occurred
-	  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	  //console.log('body:', body); // Print the HTML for the Google homepage.
+	  console.log('Request: ' + requestString);
+	  console.log('Error:', error); // Print the error if one occurred
+	  console.log('Status Code:', response && response.statusCode); // Print the response status code if a response was received
 
 	var obj = JSON.parse(body);
-	//console.log(typeof(obj));
-	console.log('-------------------');
-	console.log('status_interval: ' + obj.status_interval);
-	console.log('status_interval_nac_host_conn: ' + obj.status_interval_nac_host_conn);
-	console.log('status_interval_trx_timeout: ' + obj.status_interval_trx_timeout);
-	console.log('-------------------');
+	//console.log('-------------------');
+	console.log('Status interval (minutes): ' + obj.status_interval);
+	console.log('Host conn limit (freq.): ' + obj.host_conn_limit);
+	console.log('Status interval NAC host conn (minutes): ' + obj.status_interval_nac_host_conn);
+	console.log('Transaction timeout limit (freq.): ' + obj.trx_limit);
+	console.log('Status interval trx timeout (minutes): ' + obj.status_interval_trx_timeout);
+	//console.log('-------------------');
 	
-	//setInterval(writeMsg, obj.status_interval_nac_host_conn*1000);		
+	var fs = require('fs');
+					
+	var msg='[' + new Date().toLocaleString() + ']' + ' ' + '[alert.js]: ' + 'Send alert started' + '\n';
+	
+	fs.appendFile('Log' + today +'.log', msg , function (err) {
+	  if (err) throw err;
+	  //console.log('Log saved!');
+	});
+	
+	console.log('Checking for NAC Status...');
+	sendAlert();
+				
 	setInterval(sendAlert, obj.status_interval*60000);		
 	
 	});		
